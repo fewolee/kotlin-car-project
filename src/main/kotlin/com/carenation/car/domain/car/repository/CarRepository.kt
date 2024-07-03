bea6bacc91baa3d1c1c8c5110770e52b99d21000
fewolee <chulwoo25@naver.com>
2024-07-03 14:02:45 +0900

    [REFAC]Car, Category 엔티티 구조 변경

--- src/main/kotlin/com/carenation/car/domain/car/repository/CarRepository.kt
+++ src/main/kotlin/com/carenation/car/domain/car/repository/CarRepository.kt
@@ -1,10 +1,13 @@
 package com.carenation.car.domain.car.repository
 
 import com.carenation.car.domain.car.entity.Car
+import com.carenation.car.domain.car.enum.CarCategory
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CarRepository : JpaRepository<Car, Long> {
-    fun findByRentAvailableTrue(): List<Car>
+    fun findByCategories(category: CarCategory): List<Car> //자동차 카테고리로 검색
+    fun findByManufactureAndModelNameAndProductionYear(manufacture: String, modelName: String, productionYear: Int): List<Car>
+    // 제조사, 모델명, 생산년도로 검색
 }
