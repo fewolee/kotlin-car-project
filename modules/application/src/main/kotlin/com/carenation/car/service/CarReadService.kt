99d0fbf3100e21c0260f6b500afd03e05d025fd3
fewolee <chulwoo25@naver.com>
2024-07-23 15:13:44 +0900

    [REFAC] 데이터 조회 관련 메서드 Transactional어노테이션 readOnly= true 적용

--- modules/application/src/main/kotlin/com/carenation/car/service/CarReadService.kt
+++ modules/application/src/main/kotlin/com/carenation/car/service/CarReadService.kt
@@ -15,18 +15,18 @@ class CarReadService(
     private val carReadOutPort: CarReadOutPort,
 ) : CarReadUseCase {
     // 자동차 ID로 단건 조회
-    @Transactional
+    @Transactional(readOnly = true)
     override fun getById(carId: Long): CarModel = carReadOutPort.getById(carId)
 
     // 모든 자동차 정보 조회
-    @Transactional
+    @Transactional(readOnly = true)
     override fun getAll(): List<CarModel> = carReadOutPort.getAll()
 
     // 카테고리 이름으로 자동차 정보 조회
-    @Transactional
+    @Transactional(readOnly = true)
     override fun getByCategoryName(categoryName: String): List<CarModel> = carReadOutPort.getByCategoryName(categoryName)
 
     // 모델명, 제조사, 생상년도로 동적으로 자동차 정보를 조회
-    @Transactional
+    @Transactional(readOnly = true)
     override fun getDynamicQuery(req: CarInfoListInDto): List<CarModel> = carReadOutPort.getDynamicQuery(req)
 }
